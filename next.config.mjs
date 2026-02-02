b1ca83be297750b5959843274d3f8c98d50e5cd6
IlyaAbramov1 <126863362+IlyaAbramov1@users.noreply.github.com>
2026-02-02 16:49:52 +0400

    Fix config for Vercel and GitHub Pages builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
+const isGitHubPagesBuild = process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig = {
     output: "export",
     images: { unoptimized: true },
-    basePath: isProd ? "/biblioteka" : "",
-    assetPrefix: isProd ? "/biblioteka/" : "",
+    basePath: isGitHubPagesBuild ? "/biblioteka" : "",
+    assetPrefix: isGitHubPagesBuild ? "/biblioteka/" : "",
     trailingSlash: true,
 };
 
